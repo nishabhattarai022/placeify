2a06083352e94dc04a40076cbeef2af412c794a8
JoshiAnisha <anishajoshi2060@gmail.com>
2026-06-13 11:37:17 -0500

    files cleanup

--- placeify_flutter/lib/features/home/data/catalog_product_mapper.dart
+++ placeify_flutter/lib/features/home/data/catalog_product_mapper.dart
@@ -2,7 +2,7 @@ import 'package:placeify_client/placeify_client.dart' as api;
 
 import '../../../core/config/resolve_media_url.dart';
 import '../../cart/data/product_id_codec.dart';
-import '../../product_detail/data/product_model_3d_urls.dart';
+import '../../product_detail/data/product_3d_model_resolver.dart';
 import '../domain/models/product.dart';
 
 abstract final class CatalogProductMapper {
@@ -43,7 +43,7 @@ abstract final class CatalogProductMapper {
     final model3dUrl = product.model3dUrl?.trim();
     final has3dPreview = model3dUrl != null && model3dUrl.isNotEmpty;
     if (has3dPreview) {
-      ProductModel3dUrls.set(uiId, model3dUrl);
+      Product3dModelResolver.setModelUrl(uiId, model3dUrl);
     }
 
     return Product(
