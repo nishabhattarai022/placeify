5001cfd18503e1810d89340e3655a8a78220b411
Rosika Gajurel <rosikagajurel@Rosikas-MacBook-Air.local>
2026-06-14 00:39:06 +0545

    tested code for camera support

--- placeify_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ placeify_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,9 @@ import file_picker
 import file_selector_macos
 import flutter_secure_storage_darwin
 import flutter_web_auth_2
+import geolocator_apple
 import google_sign_in_ios
+import package_info_plus
 import shared_preferences_foundation
 import sign_in_with_apple
 import sqflite_darwin
@@ -27,7 +29,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
